ab72770bf17010a61b6f91852f2622f8994ff987
Martin Hauner <martin.hauner@gmx.net>
2012-03-31 11:42:00 +0200

    translated one example feature

---
+++ test/projects/Books/test/cucumber/ListBooks_de.feature
@@ -0,0 +1,11 @@
+# language: de
+
+Funktionalität: zeige die Liste der Bücher die ich besitze
+    Als Buchbesitzer
+    möchte ich meine Bücher auflisten
+    damit ich nachsehen kann, welche Bücher ich besitze
+
+Szenario: vorhandene Bücher auflisten
+   Wenn ich "Specification by Example" bereits hinzugefügt habe
+    Und ich die Bücherliste aufrufe
+   Dann beinhaltet die Bücherliste das Buch "Specification by Example"
---
+++ test/projects/Books/test/cucumber/step_definitions/Book_Steps_de.groovy
@@ -0,0 +1,19 @@
+this.metaClass.mixin (cucumber.runtime.groovy.DE)
+
+import pages.book.ListPage
+import data.Data
+
+
+Wenn (~"^ich die Bücherliste aufrufe\$") { ->
+    to ListPage
+    assert at (ListPage)
+}
+
+Wenn (~"^ich \"([^\"]*)\" bereits hinzugefügt habe\$") { String bookTitle ->
+    Data.createBookByTitle (bookTitle)
+}
+
+Dann (~"^beinhaltet die Bücherliste das Buch \"([^\"]*)\"\$") { String bookTitle ->
+    assert at (ListPage)
+    page.checkBookAtRow (bookTitle, 0)
+}
