b1412fe87af1e977689005be2703c3331986506b
Martin Hauner <martin.hauner@gmx.net>
2012-05-28 16:17:16 +0200

    simplified regexs

--- test/projects/Books/test/functional/steps/BookSteps.groovy
+++ test/projects/Books/test/functional/steps/BookSteps.groovy
@@ -1,20 +1,21 @@
 import books.BookController
 import data.Data
 
-this.metaClass.mixin (cucumber.runtime.groovy.EN)
+import static cucumber.runtime.groovy.EN.*
+//this.metaClass.mixin (cucumber.runtime.groovy.EN)
 
 
-Given (~"^I open the book tracker\$") { ->
+Given (~'^I open the book tracker$') { ->
     // nop
 }
 
-When (~"^I add \"([^\"]*)\"\$") { String bookTitle ->
+When (~'^I add "([^"]*)"$') { String bookTitle ->
     bookController = new BookController ()
     bookController.params << Data.findByTitle (bookTitle)
     bookController.add ()
 }
 
-Then (~"^I see \"([^\"]*)\"s details\$") { String bookTitle ->
+Then (~'^I see "([^"]*)"s details$') { String bookTitle ->
     def expected = Data.findByTitle (bookTitle)
     def actual = bookController.response.json
 
@@ -24,17 +25,17 @@ Then (~"^I see \"([^\"]*)\"s details\$") { String bookTitle ->
 }
 
 
-Given (~"^I have already added \"([^\"]*)\"\$") { String bookTitle ->
+Given (~'^I have already added "([^"]*)"$') { String bookTitle ->
     def bookService = appCtx.getBean ("bookService")
     bookService.add (Data.findByTitle (bookTitle))
 }
 
-When (~"^I view the book list\$") { ->
+When (~'^I view the book list$') { ->
     bookController = new BookController ()
     bookController.all ()
 }
 
-Then (~"^my book list contains \"([^\"]*)\"\$") { String bookTitle ->
+Then (~'^my book list contains "([^"]*)"$') { String bookTitle ->
     def expected = Data.findByTitle (bookTitle)
     def all = bookController.response.json
     actual = all.getJSONObject (0)
