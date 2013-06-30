72acc491464374c6c87ab41cc9ee9771ae80c3cc
Martin Hauner <martin.hauner@gmx.net>
2013-06-30 16:36:05 +0200

    updated version.

--- CucumberGrailsPlugin.groovy
+++ CucumberGrailsPlugin.groovy
@@ -15,7 +15,7 @@
  */
 
 class CucumberGrailsPlugin {
-    def version = "0.8.0"
+    def version = "0.9.0-SNAPSHOT"
     def grailsVersion = "1.3.0 > *"
     def dependsOn = [:]
     def pluginExcludes = [
