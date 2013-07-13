b30c25b8dafaccc1408c7654e8419bc3c4077379
Martin Hauner <martin.hauner@gmx.net>
2013-07-13 09:49:05 +0200

    referencing unknown plugin version?

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -29,7 +29,8 @@ grails.project.dependency.resolution = {
     }
 
     plugins {
-        build (":release:2.2.1", ':rest-client-builder:1.0.3') {
+        //':rest-client-builder:1.0.3'
+        build (":release:2.2.1", ':rest-client-builder:1.0.2') {
             export = false
         }
 
