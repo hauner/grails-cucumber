7d4a3f5767fc977b7dc5e8485ae0320973a67fb9
Martin Hauner <martin.hauner@gmx.net>
2014-04-14 16:15:42 +0200

    upgraded to grails 2.3.7

--- test/projects/Books_geb/grails-app/conf/BuildConfig.groovy
+++ test/projects/Books_geb/grails-app/conf/BuildConfig.groovy
@@ -1,4 +1,5 @@
 grails.servlet.version = "2.5" // Change depending on target container compliance (2.5 or 3.0)
+grails.project.work.dir = "target/work"
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
@@ -34,8 +35,8 @@ grails.project.dependency.resolution = {
         //mavenRepo "http://repository.jboss.com/maven2/"
     }
 
-    def gebVersion      = "0.7.2"
-    def seleniumVersion = "2.25.0"
+    def gebVersion      = "0.9.2"
+    def seleniumVersion = "2.41.0"
 
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
@@ -46,22 +47,29 @@ grails.project.dependency.resolution = {
         test ("org.seleniumhq.selenium:selenium-chrome-driver:$seleniumVersion")
         test ("org.seleniumhq.selenium:selenium-firefox-driver:$seleniumVersion")
 
-        test ("org.codehaus.geb:geb-junit4:$gebVersion")
+        test ("org.gebish:geb-junit4:$gebVersion")
     }
 
     plugins {
-        runtime ":hibernate:$grailsVersion"
-        runtime ":jquery:1.7.1"
-        runtime ":resources:1.1.6"
+        // plugins for the build system only
+        build ":tomcat:7.0.52.1"
 
+        // plugins for the compile step
+        compile ":scaffolding:2.0.1"
+        compile ':cache:1.1.1'
+        compile ":remote-control:1.4"
+
+        // plugins needed at runtime but not for compilation
+        runtime ":hibernate:3.6.10.12"
+        runtime ":database-migration:1.3.8"
+        runtime ":jquery:1.11.0.1"
+        runtime ":resources:1.2.7"
         // Uncomment these (or add new ones) to enable additional resources capabilities
-        //runtime ":zipped-resources:1.0"
-        //runtime ":cached-resources:1.0"
-        //runtime ":yui-minify-resources:0.1.4"
+        //runtime ":zipped-resources:1.0.1"
+        //runtime ":cached-resources:1.1"
+        //runtime ":yui-minify-resources:0.1.5"
 
-        build ":tomcat:$grailsVersion"
-
+        // test
         test ":geb:$gebVersion"
-        //test ":cucumber:0.5.0"
     }
 }
