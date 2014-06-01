4e39824f7f28bde737296035ca698bc17757f275
Martin Hauner <martin.hauner@gmx.net>
2014-06-01 18:30:12 +0200

    upgraded to grails 2.4.0

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -7,7 +7,7 @@ dataSource {
 hibernate {
     cache.use_second_level_cache = true
     cache.use_query_cache = true
-    cache.provider_class = 'org.hibernate.cache.ehcache.EhCacheRegionFactory'
+    cache.region.factory_class = 'org.hibernate.cache.ehcache.EhCacheRegionFactory'
 }
 // environment specific settings
 environments {
