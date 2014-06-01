4bc3648c3a8716fa6f5437dc6ae95315639c77d6
Martin Hauner <martin.hauner@gmx.net>
2014-06-01 15:51:54 +0200

    upgraded example to grails 2.4.0

--- test/projects/Books_remote/grails-app/conf/BuildConfig.groovy
+++ test/projects/Books_remote/grails-app/conf/BuildConfig.groovy
@@ -11,31 +11,12 @@ grails.plugin.location.cucumber = "../../.."
 
 // uncomment (and adjust settings) to fork the JVM to isolate classpaths
 
-/*
 grails.project.fork = [
     // configure settings for the run-app JVM
     run: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256],
     // configure settings for the test-app JVM, uses the daemon by default
     test: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, daemon:true]
 ]
-*/
-
-/*
-grails.project.fork = [
-    // configure settings for compilation JVM, note that if you alter the Groovy version forked compilation is required
-    //  compile: [maxMemory: 256, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
-
-    // configure settings for the test-app JVM, uses the daemon by default
-    test: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
-    // configure settings for the run-app JVM
-    run: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
-    // configure settings for the run-war JVM
-    war: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
-    // configure settings for the Console UI JVM
-    console: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256]
-]
-*/
-
 
 grails.project.dependency.resolution = {
     // inherit Grails' default dependencies
@@ -65,33 +46,44 @@ grails.project.dependency.resolution = {
     }
 
     dependencies {
-        // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes e.g.
-
-        // runtime 'mysql:mysql-connector-java:5.1.22'
+        compile "org.springframework:spring-orm:$springVersion"
+        compile "org.springframework:spring-expression:$springVersion"
+        compile "org.springframework:spring-aop:$springVersion"
 
         compile ("org.codehaus.groovy.modules.http-builder:http-builder:0.6") {
             excludes 'groovy', 'xml-apis'
         }
+
+        // spock support
+        test "cglib:cglib-nodep:3.1"
+        //test "org.objenesis:objenesis:1.2"
     }
 
     plugins {
         // plugins for the build system only
-        build ":tomcat:7.0.52.1"
+        build ":tomcat:7.0.53"
         //build ':jetty:2.0.3'
 
         // plugins for the compile step
-        compile ":scaffolding:2.0.1"
-        compile ':cache:1.1.1'
+        compile ":scaffolding:2.1.0"
+        //compile ':cache:1.1.6'
+        //compile ":asset-pipeline:1.8.7"
         compile ":remote-control:1.4"
 
         // plugins needed at runtime but not for compilation
-        runtime ":hibernate:3.6.10.12"
-        runtime ":database-migration:1.3.8"
-        runtime ":jquery:1.11.0.1"
-        runtime ":resources:1.2.7"
+        runtime ":hibernate4:4.3.5.3" // or ":hibernate:3.6.10.15"
+        runtime ":database-migration:1.4.0"
+        runtime ":jquery:1.11.1"
+        runtime ":resources:1.2.8"
         // Uncomment these (or add new ones) to enable additional resources capabilities
         //runtime ":zipped-resources:1.0.1"
         //runtime ":cached-resources:1.1"
         //runtime ":yui-minify-resources:0.1.5"
+
+        // Uncomment these to enable additional asset-pipeline capabilities
+        //compile ":sass-asset-pipeline:1.7.4"
+        //compile ":less-asset-pipeline:1.7.0"
+        //compile ":coffee-asset-pipeline:1.7.0"
+        //compile ":handlebars-asset-pipeline:1.3.0.3"
     }
 }
