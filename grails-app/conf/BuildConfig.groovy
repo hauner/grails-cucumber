867c969b8329a5ed0645d5839960536f147025a7
Martin Hauner <martin.hauner@gmx.net>
2014-08-23 10:29:36 +0200

    #37 #38 removed all unnecessary dependencies to avoid "spamming" unwanted dependencies into projects that use the plugin.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -16,14 +16,9 @@ grails.project.fork = [
 
 grails.project.dependency.resolver = "maven"
 grails.project.dependency.resolution = {
+    inherits("global")
 
-    // inherit Grails' default dependencies
-    inherits("global") {
-        // uncomment to disable ehcache
-        // excludes 'ehcache'
-    }
-
-    log "warn" // Ivy resolver: 'error', 'warn', 'info', 'debug' or 'verbose'
+    log "warn"
 
     repositories {
         grailsPlugins()
@@ -34,44 +29,20 @@ grails.project.dependency.resolution = {
     }
 
     dependencies {
-        compile "org.springframework:spring-orm:$springVersion"
-
         // cucumber
         compile ("info.cukes:cucumber-groovy:1.1.7") {
-           excludes 'ant'   // avoid ant version conflict
+           excludes 'ant' // avoid ant version conflict
         }
 
         // spock support
-        test "cglib:cglib-nodep:3.1"
-        //test "org.objenesis:objenesis:1.2"
+        test ("cglib:cglib-nodep:3.1") {
+            export = false
+        }
     }
 
     plugins {
-        // plugins for the build system only
-        build ":tomcat:7.0.53"
-        build ":release:3.0.1"
-//        build (":release:2.2.1", ':rest-client-builder:1.0.3') {
-//            export = false
-//        }
-
-        // plugins for the compile step
-        compile ":scaffolding:2.1.0"
-        //compile ':cache:1.1.6'
-        //compile ":asset-pipeline:1.8.7"
-
-        // plugins needed at runtime but not for compilation
-        runtime ":hibernate4:4.3.5.3" // or ":hibernate:3.6.10.15"
-        runtime ":database-migration:1.4.0"
-        //runtime ":jquery:1.10.2.2"
-        runtime ":resources:1.2.8"
-        // Uncomment these (or add new ones) to enable additional resources capabilities
-        //runtime ":zipped-resources:1.0.1"
-        //runtime ":cached-resources:1.1"
-
-        // Uncomment these to enable additional asset-pipeline capabilities
-        //compile ":sass-asset-pipeline:1.7.4"
-        //compile ":less-asset-pipeline:1.7.0"
-        //compile ":coffee-asset-pipeline:1.7.0"
-        //compile ":handlebars-asset-pipeline:1.3.0.3"
+        build (":tomcat:7.0.53", ":release:3.0.1") {
+            export = false
+        }
     }
 }
