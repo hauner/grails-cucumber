1311797475ac15bfd78922286f1d198371ad6bb7
Martin Hauner <martin.hauner@gmx.net>
2014-08-23 18:44:21 +0200

    upgraded grails.

--- test/projects/Books_remote/grails-app/conf/BuildConfig.groovy
+++ test/projects/Books_remote/grails-app/conf/BuildConfig.groovy
@@ -18,6 +18,7 @@ grails.project.fork = [
     test: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, daemon:true]
 ]
 
+grails.project.dependency.resolver = "maven"
 grails.project.dependency.resolution = {
     // inherit Grails' default dependencies
     inherits("global") {
@@ -26,29 +27,22 @@ grails.project.dependency.resolution = {
     }
     log "error" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
     checksums true // Whether to verify checksums on resolve
-    legacyResolve false // whether to do a secondary resolve on plugin installation, not advised and here for backwards compatibility
 
     repositories {
-        inherits true // Whether to inherit repository definitions from plugins
-
         grailsPlugins()
         grailsHome()
-        grailsCentral()
-
         mavenLocal()
+        grailsCentral()
         mavenCentral()
-
-        // uncomment these (or add new ones) to enable remote dependency resolution from public Maven repositories
-        //mavenRepo "http://snapshots.repository.codehaus.org"
-        //mavenRepo "http://repository.codehaus.org"
-        //mavenRepo "http://download.java.net/maven/2/"
-        //mavenRepo "http://repository.jboss.com/maven2/"
     }
 
     dependencies {
-        compile "org.springframework:spring-orm:$springVersion"
-        compile "org.springframework:spring-expression:$springVersion"
-        compile "org.springframework:spring-aop:$springVersion"
+         // runtime 'mysql:mysql-connector-java:5.1.29'
+         // runtime 'org.postgresql:postgresql:9.3-1101-jdbc41'
+
+//        compile "org.springframework:spring-orm:$springVersion"
+//        compile "org.springframework:spring-expression:$springVersion"
+//        compile "org.springframework:spring-aop:$springVersion"
 
         compile ("org.codehaus.groovy.modules.http-builder:http-builder:0.6") {
             excludes 'groovy', 'xml-apis'
@@ -56,34 +50,25 @@ grails.project.dependency.resolution = {
 
         // spock support
         test "cglib:cglib-nodep:3.1"
-        //test "org.objenesis:objenesis:1.2"
     }
 
     plugins {
         // plugins for the build system only
-        build ":tomcat:7.0.53"
-        //build ':jetty:2.0.3'
+        build ":tomcat:7.0.55"
 
         // plugins for the compile step
-        compile ":scaffolding:2.1.0"
-        //compile ':cache:1.1.6'
-        //compile ":asset-pipeline:1.8.7"
+        compile ":scaffolding:2.1.2"
+        //compile ':cache:1.1.7'
+        //compile ":asset-pipeline:1.9.6"
         compile ":remote-control:1.4"
 
         // plugins needed at runtime but not for compilation
-        runtime ":hibernate4:4.3.5.3" // or ":hibernate:3.6.10.15"
-        runtime ":database-migration:1.4.0"
-        runtime ":jquery:1.11.1"
-        runtime ":resources:1.2.8"
-        // Uncomment these (or add new ones) to enable additional resources capabilities
-        //runtime ":zipped-resources:1.0.1"
-        //runtime ":cached-resources:1.1"
-        //runtime ":yui-minify-resources:0.1.5"
-
-        // Uncomment these to enable additional asset-pipeline capabilities
-        //compile ":sass-asset-pipeline:1.7.4"
-        //compile ":less-asset-pipeline:1.7.0"
-        //compile ":coffee-asset-pipeline:1.7.0"
+        runtime ":hibernate4:4.3.5.5" // or ":hibernate:3.6.10.17"
+        //runtime ":database-migration:1.4.0"
+        //runtime ":jquery:1.11.1"
+        //compile ":sass-asset-pipeline:1.9.0"
+        //compile ":less-asset-pipeline:1.10.0"
+        //compile ":coffee-asset-pipeline:1.8.0"
         //compile ":handlebars-asset-pipeline:1.3.0.3"
     }
 }
--- test/projects/Books_remote/grails-app/conf/DataSource.groovy
+++ test/projects/Books_remote/grails-app/conf/DataSource.groovy
@@ -8,6 +8,8 @@ hibernate {
     cache.use_second_level_cache = true
     cache.use_query_cache = false
     cache.region.factory_class = 'org.hibernate.cache.ehcache.EhCacheRegionFactory'
+    singleSession = true // configure OSIV singleSession mode
+    flush.mode = 'manual' // OSIV session flush mode outside of transactional context
 }
 // environment specific settings
 environments {
