acc0dd74438ac1dfd9db5edda455604a4bbce205
Martin Hauner <martin.hauner@gmx.net>
2014-08-23 18:42:07 +0200

    upgraded grails.

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -4,11 +4,16 @@ dataSource {
     username = "sa"
     password = ""
 }
+
 hibernate {
     cache.use_second_level_cache = true
     cache.use_query_cache = true
     cache.region.factory_class = 'org.hibernate.cache.ehcache.EhCacheRegionFactory'
+
+    singleSession = true // configure OSIV singleSession mode
+    flush.mode = 'manual' // OSIV session flush mode outside of transactional context
 }
+
 // environment specific settings
 environments {
     development {
