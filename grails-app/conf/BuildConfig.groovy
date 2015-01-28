9723668fd6ab89d2ecd702adc4f7cd32cffab2b1
Martin Hauner <martin.hauner@gmx.net>
2015-01-28 21:43:32 +0100

    new cucumber-jvm.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -30,7 +30,7 @@ grails.project.dependency.resolution = {
 
     dependencies {
         // cucumber
-        compile ("info.cukes:cucumber-groovy:1.2.0") {
+        compile ("info.cukes:cucumber-groovy:1.2.2") {
            excludes 'ant' // avoid ant version conflict
         }
 
